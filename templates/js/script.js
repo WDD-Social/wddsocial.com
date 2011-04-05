6350c555f68971936b98fbfae9b5dc74bd40a829
Anthony Colangelo <me@acolangelo.com>
2011-04-04 22:23:23 -0400

    Added a 'Load More' button at the end of the activity feed in the user's dashboard, and styled that. Created a filter proof-of-concept on the activity feed, using some slick jQuery.

--- templates/js/script.js
+++ templates/js/script.js
@@ -1,26 +1,15 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(function() {
+	$('.filters a').live('click',function(){
+		$(this).parent().find('a.current').removeClass('current');
+		$(this).addClass('current');
+		if($(this).html() === 'All'){
+			$(this).parent().parent().find('article').slideDown(500,'easeInOutQuad');
+		}else{
+			var class = $(this).text().toLowerCase();
+			$(this).parent().parent()
+				.find('article.'+class).slideDown(500,'easeInOutQuad').parent()
+				.find('article:not(article.'+class+')').slideUp(500,'easeInOutQuad');
+		}
+		return false;
+	});
+});
