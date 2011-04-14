27cb1b2fbdfac2926a77e65de9f131b45e8b151a
Anthony Colangelo <me@acolangelo.com>
2011-04-14 13:25:03 -0400

    Imported site images, CSS, and JS files into the html directory from the templates directory.

--- html/js/plugins.js
+++ html/js/plugins.js
@@ -1,9 +1,34 @@
+
+(function($){
+
+ 
+
+
+
+
+
+ 
+
+
+
+})(this.jQuery);
+
+
+
+
 window.log = function(){
-  log.history = log.history || [];  
+  log.history = log.history || [];   
   log.history.push(arguments);
-  arguments.callee = arguments.callee.caller;  
-  if(this.console) console.log( Array.prototype.slice.call(arguments) );
+  if(this.console){
+    console.log( Array.prototype.slice.call(arguments) );
+  }
 };
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
+(function(doc){
+  var write = doc.write;
+  doc.write = function(q){ 
+    log('document.write(): ',arguments); 
+    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
+  };
+})(document);
 
 
