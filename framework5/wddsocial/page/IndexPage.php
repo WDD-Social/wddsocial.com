bcdc1086e5f86295ae956c1ae6664a0774aae27d
Tyler Matthews <tmatthewsdev@gmail.com>
2011-04-15 22:01:22 -0400

    sample inline query

--- framework5/wddsocial/page/IndexPage.php
+++ framework5/wddsocial/page/IndexPage.php
@@ -30,6 +30,33 @@ class IndexPage implements \Framework5\IExecutable {
 		
 		
 		echo render('wddsocial.view.TemplateView', array('section' => 'top', 'title' => 'Connecting the Full Sail University Web Community'));
+		
+		
+		$query = $db->query("
+			SELECT p.id, title, description, p.vanityURL, p.datetime, 'project' AS `type`, u.id AS userID, CONCAT_WS(' ', firstName, lastName) AS userName, u.vanityURL AS userURL,
+			getDateDiffEN(p.datetime) AS `date`
+			FROM projects AS p
+			LEFT JOIN users AS u ON (p.userID = u.id)
+			UNION
+			SELECT a.id, a.title, a.description, a.vanityURL, a.datetime, 'article' AS `type`, u.id AS userID, CONCAT_WS(' ', firstName, lastName) AS userName, u.vanityURL AS userURL, getDateDiffEN(a.datetime) AS `DATE`
+			FROM articles AS a
+			LEFT JOIN users AS u ON (a.userID = u.id)
+			UNION
+			SELECT id, CONCAT_WS(' ', firstName, lastName) AS title, bio AS description, vanityURL, `DATETIME`, 'person' AS `TYPE`, id AS userID, CONCAT_WS(' ', firstName, lastName) AS userName, vanityURL AS userURL, getDateDiffEN(`DATETIME`) AS `DATE`
+			FROM users AS u
+			ORDER BY DATETIME DESC
+		");
+		$query->setFetchMode(\PDO::FETCH_OBJ);
+		while($row = $query->fetch()){  
+			echo "<p>{$row->type}</p>";
+			echo "<p>{$row->title}</p>";
+			echo "<p>{$row->description}</p>";
+			echo "<p>{$row->vanityURL}</p>";
+			echo "<p>{$row->userName}</p>";
+			echo "<p>{$row->date}</p>";
+			echo "<p>------------------------------------</p>";
+		}
+		
 		echo render('wddsocial.view.TemplateView', array('section' => 'bottom'));
 		
 	}
