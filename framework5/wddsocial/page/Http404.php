f1a72205e025819205de33fbc1f584bfe4e289e9
Tyler Matthews <tmatthewsdev@gmail.com>
2011-04-15 20:55:06 -0400

    added base template to 404 page

--- framework5/wddsocial/page/Http404.php
+++ framework5/wddsocial/page/Http404.php
@@ -10,6 +10,9 @@ namespace WDDSocial;
 class Http404 implements \Framework5\IExecutable {
 
 	public static function execute() {
-		echo "{404 page}<br/>";
+		echo render('wddsocial.view.TemplateView', array('section' => 'top', 'title' => '404: Page not found'));
+		echo '404';
+		//echo render('wddsocial.view.Http404View');
+		echo render('wddsocial.view.TemplateView', array('section' => 'bottom'));
 	}
 }
