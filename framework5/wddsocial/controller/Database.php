5defbc1d8544fcc586ba61d5047118aeeab965d3
Anthony Colangelo <me@acolangelo.com>
2011-04-17 23:57:57 -0400

    Added SQL and DB files to live branch of WDD Social.

---
+++ framework5/wddsocial/controller/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace WDDSocial;
+
+/*
+* PDO Database controller
+* @author tmatthews (tmatthewsdev@gmail.com)
+*/
+
+final class Database extends \PDO {
+	
+	public function __construct() {
+		
+		# call PDO construct
+		parent::__construct("mysql:host=localhost;dbname=wddsocial", "root", "root");
+		
+		# set default attributes
+		$this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+	}
+}
