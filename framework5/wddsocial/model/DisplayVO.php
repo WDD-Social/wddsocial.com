0ff0302aec87eb96b195d05e26ff63ffc526233a
Anthony Colangelo <me@acolangelo.com>
2011-04-19 12:39:34 -0400

    Created ImageVO, modified UserVO to work with PDO::FETCH_CLASS. Implemented ImageVO into DisplayVO logic.

--- framework5/wddsocial/model/DisplayVO.php
+++ framework5/wddsocial/model/DisplayVO.php
@@ -16,16 +16,17 @@ class DisplayVO{
 		$this->sql = new SelectorSQL();
 		
 		if($type != 'person'){
-			$this->getCommentsCount();
+			$this->get_comments_count();
 		}
 		
-		$this->getTags();
+		$this->get_tags();
 		if($this->type == 'project' || $this->type == 'article'){
-			$this->getTeam();
+			$this->get_team();
+			$this->get_images();
 		}
 	}
 	
-	private function getCommentsCount(){
+	private function get_comments_count(){
 		$data = array('id' => $this->id);
 		switch ($this->type){
 			case 'project':
@@ -59,7 +60,7 @@ class DisplayVO{
 		}
 	}
 	
-	private function getTags(){
+	private function get_tags(){
 		$data = array('id' => $this->id);
 		switch ($this->type){
 			case 'project':
@@ -113,32 +114,49 @@ class DisplayVO{
 		}
 	}
 	
-	private function getTeam(){
+	private function get_team(){
 		import('wddsocial.model.UserVO');
 		$data = array('id' => $this->id);
 		switch ($this->type){
 			case 'project':
 				$query = $this->db->prepare($this->sql->getProjectTeam);
 				$query->execute($data);
-				while($row = $query->fetch(\PDO::FETCH_OBJ)){
-					$user = new UserVO();
-					$user->userID = $row->id;
-					$user->firstName = $row->firstName;
-					$user->lastName = $row->lastName;
-					$user->vanityURL = $row->vanityURL;
+				$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\UserVO');
+				while($user = $query->fetch()){
 					array_push($this->team,$user);
 				}
 				break;
 			case 'article':
 				$query = $this->db->prepare($this->sql->getArticleTeam);
 				$query->execute($data);
-				while($row = $query->fetch(\PDO::FETCH_OBJ)){
-					$user = new UserVO();
-					$user->userID = $row->id;
-					$user->firstName = $row->firstName;
-					$user->lastName = $row->lastName;
-					$user->vanityURL = $row->vanityURL;
+				$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\UserVO');
+				while($user = $query->fetch()){
 					array_push($this->team,$user);
+				}
+				break;
+			default :
+				break;
+		}
+	}
+	
+	private function get_images(){
+		import('wddsocial.model.ImageVO');
+		$data = array('id' => $this->id);
+		switch ($this->type){
+			case 'project':
+				$query = $this->db->prepare($this->sql->getProjectImages);
+				$query->execute($data);
+				$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\ImageVO');
+				while($image = $query->fetch()){
+					array_push($this->images,$image);
+				}
+				break;
+			case 'article':
+				$query = $this->db->prepare($this->sql->getArticleImages);
+				$query->execute($data);
+				$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\ImageVO');
+				while($image = $query->fetch()){
+					array_push($this->images,$image);
 				}
 				break;
 			default :
---
+++ framework5/wddsocial/model/ImageVO.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WDDSocial;
+/*
+*
+* Author: Anthony Colangelo (me@acolangelo.com)
+*
+*/
+class ImageVO{
+	public $id, $title, $description, $file;
+	
+	public function __construct(){
+		
+	}
+}
