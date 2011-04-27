5d3896040e4ce24e86253e5fa95bf5aa55682669
Anthony Colangelo <me@acolangelo.com>
2011-04-27 14:18:05 -0400

    Created ContentVO, to be used in content pages such as project page, article page, event page, etc.

---
+++ framework5/wddsocial/model/ContentVO.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WDDSocial;
+/*
+* 
+* 
+* @author: Anthony Colangelo (me@acolangelo.com)
+*/
+
+class ContentVO {
+	/* public $id, $language, $firstName, $lastName, $avatar, $bio, $hometown, $age, $type, $contact = array(), $email, $fullsailEmail, $vanityURL, $extra = array(); */
+	
+	public function __construct(){
+		
+	}
+	
+}
