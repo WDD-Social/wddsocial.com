8285bfa57babb8e831bbdc4c17a25812a3268ab9
Anthony Colangelo <me@acolangelo.com>
2011-05-01 17:17:30 -0400

    Created queries to check uniqueness of VanityURL's, email addresses, and Full Sail email addresses.

--- framework5/wddsocial/sql/ValidatorSQL.php
+++ framework5/wddsocial/sql/ValidatorSQL.php
@@ -17,7 +17,22 @@ class ValidatorSQL{
 		'getArticleOwners' => "
 			SELECT userID
 			FROM userArticles
-			WHERE articleID = :id"
+			WHERE articleID = :id",
+		
+		'checkIfVanityURLExists' => "
+			SELECT COUNT(*) AS `count`
+			FROM users
+			WHERE vanityURL = :vanityURL",
+		
+		'checkIfEmailExists' => "
+			SELECT COUNT(*) AS `count`
+			FROM users
+			WHERE email = :email",
+		
+		'checkIfFullSailEmailExists' => "
+			SELECT COUNT(*) AS `count`
+			FROM users
+			WHERE fullsailEmail = :fullsailEmail"
 	);
 	
 	public function __get($id){
