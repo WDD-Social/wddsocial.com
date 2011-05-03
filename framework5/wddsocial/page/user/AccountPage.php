571fafa0b073eb25c4d74d670f12b71a6b310d8e
Anthony Colangelo <me@acolangelo.com>
2011-05-03 09:27:52 -0400

    Began account page.

--- framework5/wddsocial/page/user/AccountPage.php
+++ framework5/wddsocial/page/user/AccountPage.php
@@ -13,13 +13,27 @@ class AccountPage implements \Framework5\IExecutable {
 		
 		# display site header
 		echo render('wddsocial.view.WDDSocial\TemplateView', array('section' => 'top', 'title' => 'My Account'));
+			
+		# open content section
+		echo render('wddsocial.view.WDDSocial\SectionView', array('section' => 'begin_content'));
+		
+		# display account form
+		echo render('wddsocial.view.form.WDDSocial\AccountView', array('user' => $_SESSION['user']));
+			
+		# end content section
+		echo render('wddsocial.view.WDDSocial\SectionView', array('section' => 'end_content'));
+		
+		# display site footer
+		echo render('wddsocial.view.WDDSocial\TemplateView', array('section' => 'bottom'));
 		
 		echo "<pre>";
 		print_r($_SESSION['user']);
 		echo "</pre>";
-		
-		# display site footer
-		echo render('wddsocial.view.WDDSocial\TemplateView', array('section' => 'bottom'));
+	}
+	
+	
+	
+	public static function process_form(){
 		
 	}
 }
