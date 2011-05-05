0f0539a2c90002e068dbeb97a13484e8dd692589
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-05 15:11:16 -0400

    Broke CommentDisplayView from ContentView, oop ftw.

--- framework5/wddsocial/view/ContentView.php
+++ framework5/wddsocial/view/ContentView.php
@@ -28,8 +28,6 @@ class ContentView implements \Framework5\IView {
 				return static::job_details($options['content']);
 			case 'media':
 				return static::media($options['content'],$options['active']);
-			case 'comments':
-				return static::comments($options['comments']);
 			default:
 				throw new \Framework5\Exception("ContentView requires parameter section (overview, members, media, or comments), '{$options['section']}' provided");
 		}
@@ -500,97 +498,6 @@ HTML;
 
 					</div><!-- END $active -->
 HTML;
-		
-		return $html;
-	}
-	
-	
-	
-	/**
-	* Display content comments
-	*/
-	
-	private static function comments($comments){
-		$root = \Framework5\Request::root_path();
-		$html = "";
-		$commentCount = count($comments);
-		$commentVerbage = 'comment';
-		if($commentCount > 1 || $commentCount < 1){
-			$commentVerbage .= 's';
-		}
-		$html .= <<<HTML
-
-					<div class="secondary">
-						<p>{$commentCount} {$commentVerbage}</p> 
-					</div><!-- END SECONDARY -->
-HTML;
-		
-		if($commentCount > 0){
-			foreach($comments as $comment){
-				$userVerbage = NaturalLanguage::view_profile($comment->userID,"{$comment->firstName} {$comment->lastName}");
-				$userDisplayName = NaturalLanguage::display_name($comment->userID,"{$comment->firstName} {$comment->lastName}");
-				
-				$html .= <<<HTML
-
-					<article class="with-secondary">
-HTML;
-				if(UserSession::is_current($comment->userID)){
-					$html .= <<<HTML
-
-						<div class="secondary">
-							<a href="{$root}" title="Edit Your Comment" class="edit">Edit</a> 
-							<a href="{$root}" title="Delete Your Comment" class="delete">Delete</a>
-						</div><!-- END SECONDARY -->
-HTML;
-				}else if(UserSession::is_authorized()){
-					$possessive = NaturalLanguage::possessive("{$comment->firstName} {$comment->lastName}");
-					$html .= <<<HTML
-
-						<div class="secondary">
-							<a href="{$root}" title="Flag {$possessive} Comment" class="flag">Flag</a>
-						</div><!-- END SECONDARY -->
-HTML;
-				}
-				$html .= <<<HTML
-						
-						<p class="item-image"><a href="{$root}user/{$comment->vanityURL}" title="{$userVerbage}"><img src="{$root}images/avatars/{$comment->avatar}_medium.jpg" alt="{$userDisplayName}"/></a></p>
-						<h2><a href="{$root}user/{$comment->vanityURL}" title="{$userVerbage}">{$userDisplayName}</a></h2>
-						<p>{$comment->content}</p>
-						<p class="comments">{$comment->date}</p>
-					</article>
-HTML;
-			}
-		}else{
-			if(UserSession::is_authorized()){
-				$html .= <<<HTML
-
-					<p class="empty">No one has commented yet, why don&rsquo;t you start the conversation?</p>
-HTML;
-			}
-		}
-		
-		if(UserSession::is_authorized()){
-			$user = $_SESSION['user'];
-			$userVerbage = NaturalLanguage::view_profile($user->id,"{$user->firstName} {$user->lastName}");
-			$userDisplayName = NaturalLanguage::display_name($user->id,"{$user->firstName} {$user->lastName}");
-			
-			$html .= <<<HTML
-
-					<article>
-						<p class="item-image"><a href="{$root}user/{$user->vanityURL}" title="{$userVerbage}"><img src="{$root}images/avatars/{$user->avatar}_medium.jpg" alt="{$userDisplayName}"/></a></p>
-						<h2><a href="{$root}user/{$user->vanityURL}" title="{$userVerbage}">{$userDisplayName}</a></h2>
-HTML;
-			$html .= render('wddsocial.view.form.WDDSocial\CommentView');
-			$html .= <<<HTML
-
-					</article>
-HTML;
-		}else{
-			$html .= <<<HTML
-
-					<p class="empty">You must be signed in to add a comment. <a href="{$root}signin" title="Sign In to WDD Social">Would you like to sign in?</a></p>
-HTML;
-		}
 		
 		return $html;
 	}
