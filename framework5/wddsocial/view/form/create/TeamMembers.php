10b7db1ee689fb7b095d540bc8c746d0364fa5be
Anthony Colangelo <me@acolangelo.com>
2011-05-05 12:00:32 -0400

    Added the 'Project Role' field to project team members

--- framework5/wddsocial/view/form/create/TeamMembers.php
+++ framework5/wddsocial/view/form/create/TeamMembers.php
@@ -10,24 +10,32 @@ namespace WDDSocial;
 class TeamMembers implements \Framework5\IView {		
 	
 	public static function render($options = null) {
+		$singular = rtrim($options['header'],'s');
 		$html = <<<HTML
 
-						<h1>$options</h1>
-						<fieldset>
+						<h1>{$options['header']}</h1>
 HTML;
-		for ($i = 0; $i < 3; $i++) {
-			if ($i > 0) {
-				$id = 'team' . $i;
-			}
-			else {
-				$id = 'team';
+		for ($i = 1; $i < 4; $i++) {
+			$id = 'team' . $i;
+			$html .= <<<HTML
+
+						<fieldset>
+							<label for="team$i">$singular $i</label>
+							<input type="text" name="team[]" id="team$i" />
+HTML;
+			if ($options['type'] == 'project') {
+				$html .= <<<HTML
+
+							<label for="role$i">Project Role</label>
+							<input type="text" name="roles[]" id="role$i" />
+HTML;
+				
 			}
 			$html .= <<<HTML
 
-							<input type="text" name="team[]" id="$id" />
+						</fieldset>
 HTML;
 		}
-		$singular = rtrim($options,'s');
 		$html .= <<<HTML
 
 							<a href="#" title="Add Another $singular" class="add-more">Add Another $singular</a>
