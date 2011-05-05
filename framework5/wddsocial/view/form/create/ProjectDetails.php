bbc89f2897ffdba3bfd25f964f5c9747380ef43c
Anthony Colangelo <me@acolangelo.com>
2011-05-05 01:20:30 -0400

    Created ArticleDetails form view, updated ProjectDetails.

--- framework5/wddsocial/view/form/create/ProjectDetails.php
+++ framework5/wddsocial/view/form/create/ProjectDetails.php
@@ -10,10 +10,8 @@ namespace WDDSocial;
 class ProjectDetails implements \Framework5\IView {		
 	
 	public static function render($options = null) {
-		$root = \Framework5\Request::root_path();
 		return <<<HTML
 
-						<h1>Project Details</h1>
 						<fieldset>
 							<label for="completed-date">When was this project completed?</label>
 							<input type="date" name="completed-date" id="completed-date" />
