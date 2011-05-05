d0617f30b836f2bbfd39d9a8498d8d6fc622829c
Anthony Colangelo <me@acolangelo.com>
2011-05-05 16:25:49 -0400

    Created EventExtraInputs

---
+++ framework5/wddsocial/view/form/create/EventExtraInputs.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WDDSocial;
+
+/*
+* 
+* @author Anthony Colangelo (me@acolangelo.com)
+*/
+
+class EventExtraInputs implements \Framework5\IView {		
+	
+	public static function render($options = null) {
+		$html = <<<HTML
+
+						<fieldset>
+							<label for="location">Where is it at?</label>
+							<input type="text" name="location" id="location" />
+						</fieldset>
+						<fieldset>
+							<label for="date">What day?</label>
+							<input type="date" name="date" id="date" />
+						</fieldset>
+						<fieldset>
+							<label for="start-time">When does it start?</label>
+							<input type="time" name="start-time" id="start-time" />
+							<small>Full Sail style, <strong>24-hour</strong> time</small>
+						</fieldset>
+						<fieldset>
+							<label for="duration">How long will it last?</label>
+							<input type="number" name="duration" id="duration" />
+							<small>In <strong>hours</strong>, please</small>
+						</fieldset>
+HTML;
+		
+		$html .= render('wddsocial.view.form.pieces.WDDSocial\PrivacyLevelSelector',1);
+		
+		return $html;
+	}
+}
