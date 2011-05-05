ef8b2e4c0c02bee4bc94720d487359426aa638ed
Anthony Colangelo <me@acolangelo.com>
2011-05-05 12:07:23 -0400

    Now labels for Team Members only appear when necessary (project create form).

--- framework5/wddsocial/view/form/create/TeamMembers.php
+++ framework5/wddsocial/view/form/create/TeamMembers.php
@@ -15,32 +15,42 @@ class TeamMembers implements \Framework5\IView {
 
 						<h1>{$options['header']}</h1>
 HTML;
-		for ($i = 1; $i < 4; $i++) {
-			$id = 'team' . $i;
+		if ($options['type'] != 'project') {
 			$html .= <<<HTML
+
+						<fieldset>
+HTML;
+		}
+		for ($i = 1; $i < 4; $i++) {
+			if ($options['type'] == 'project') {
+				$html .= <<<HTML
 
 						<fieldset>
 							<label for="team$i">$singular $i</label>
 							<input type="text" name="team[]" id="team$i" />
-HTML;
-			if ($options['type'] == 'project') {
-				$html .= <<<HTML
-
+							
 							<label for="role$i">Project Role</label>
 							<input type="text" name="roles[]" id="role$i" />
+						</fieldset>
 HTML;
-				
 			}
+			else {
+				$html .= <<<HTML
+
+							<input type="text" name="team[]" id="team$i" />
+HTML;
+			}
+		}
+		$html .= <<<HTML
+
+							<a href="#" title="Add Another $singular" class="add-more {$options['type']}">Add Another $singular</a>
+HTML;
+		if ($options['type'] != 'project') {
 			$html .= <<<HTML
 
 						</fieldset>
 HTML;
 		}
-		$html .= <<<HTML
-
-							<a href="#" title="Add Another $singular" class="add-more">Add Another $singular</a>
-						</fieldset>
-HTML;
 		return $html;
 	}
 }
