249429affb4d4df5adce43e579fbd6536eb8231c
Anthony Colangelo <me@acolangelo.com>
2011-05-06 12:13:21 -0400

    Wrote basic insert queries for projects, articles, events, and jobs. Wrote .ics file UID generator for events. Wrote basic vanityURL generator for projects, articles, events, and jobs.

--- framework5/wddsocial/sql/AdminSQL.php
+++ framework5/wddsocial/sql/AdminSQL.php
@@ -9,14 +9,77 @@ namespace WDDSocial;
 */
 class AdminSQL{
 	private $_info = array(
+		
+		/**
+		* User queries
+		*/
+		
 		'addUser' => "
-			INSERT INTO users
-			SET typeID = :typeID, firstName = :firstName, lastName = :lastName, email = :email, fullsailEmail = :fullsailEmail, `password` = MD5(:password), vanityURL = :vanityURL, bio = :bio, hometown = :hometown, birthday = :birthday, `datetime` = NOW()",
+			INSERT INTO users (typeID, firstName, lastName, email, fullsailEmail, `password`, vanityURL, bio, hometown, birthday, `datetime`)
+			VALUES (:typeID, :firstName, :lastName, :email, :fullsailEmail, MD5(:password), :vanityURL, :bio, :hometown, :birthday, NOW())",
 		
 		'addUserAvatar' => "
 			UPDATE users
 			SET avatar = MD5(:avatar)
+			WHERE id = :id",
+		
+		/**
+		* Project Queries
+		*/
+		
+		'addProject' => "
+			INSERT INTO projects (userID, title, description, content, vanityURL, completeDate, `datetime`)
+			VALUES (:userID, :title, :description, :content, :vanityURL, :completeDate, NOW())",
+		
+		'generateProjectVanityURL' => "
+			UPDATE projects
+			SET vanityURL = SUBSTRING(MD5(CONCAT('project',id)),1,6)
+			WHERE id = :id",
+		
+		/**
+		* Article Queries
+		*/
+		
+		'addArticle' => "
+			INSERT INTO articles (userID, privacyLevelID, title, description, content, vanityURL, `datetime`)
+			VALUES (:userID, :privacyLevelID, :title, :description, :content, :vanityURL, NOW())",
+		
+		'generateArticleVanityURL' => "
+			UPDATE articles
+			SET vanityURL = SUBSTRING(MD5(CONCAT('article',id)),1,6)
+			WHERE id = :id",
+		
+		/**
+		* Event Queries
+		*/
+		
+		'addEvent' => "
+			INSERT INTO events (userID, privacyLevelID, title, description, content, vanityURL, location, startDatetime, endDatetime, `datetime`)
+			VALUES (:userID, :privacyLevelID, :title, :description, :content, :vanityURL, :location, :startDatetime, :endDatetime, NOW())",
+		
+		'generateEventVanityURL' => "
+			UPDATE events
+			SET vanityURL = SUBSTRING(MD5(CONCAT('event',id)),1,6)
+			WHERE id = :id",
+		
+		'generateEventUID' => "
+			UPDATE events
+			SET icsUID = MD5(CONCAT(id,title,DATE_FORMAT(`datetime`, '%Y%m%dT%H%i%SZ'),'@wddsocial.com'))
+			WHERE id = :id",
+		
+		/**
+		* Job Queries
+		*/
+		
+		'addJob' => "
+			INSERT INTO jobs (userID, typeID, title, description, content, vanityURL, company, email, location, avatar, website, compensation, `datetime`)
+			VALUES (:userID, :typeID, :title, :description, :content, :vanityURL, :company, :email, :location, :avatar, :website, :compensation, NOW())",
+		
+		'generateJobVanityURL' => "
+			UPDATE jobs
+			SET vanityURL = SUBSTRING(MD5(CONCAT('job',id)),1,6)
 			WHERE id = :id"
+		
 	);
 	
 	public function __get($id){
