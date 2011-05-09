4592df9e7ca41f03abf26cce5410f3cc78578c2b
Anthony Colangelo <me@acolangelo.com>
2011-05-09 16:08:05 -0400

    Fixed an unresolved merge conflict. Left over nasty looking stuff.

--- framework5/wddsocial/view/content/EventLocationDisplayView.php
+++ framework5/wddsocial/view/content/EventLocationDisplayView.php
@@ -24,11 +24,7 @@ class EventLocationDisplayView implements \Framework5\IView {
 		if (UserSession::is_current($content->userID)) {
 			$html .= <<<HTML
 					<div class="secondary icons">
-<<<<<<< HEAD
-						<a href="{$root}" title="{$this->lang->text('owner_edit_title', $possessiveTitle)}" class="edit">{$this->lang->text('edit')}</a>
-=======
-						<a href="/" title="Edit {$possessiveTitle} Location and Time" class="edit">Edit</a>
->>>>>>> cbf38e17b883108573d9660dcd89dc19f83fc205
+						<a href="/" title="{$this->lang->text('owner_edit_title', $possessiveTitle)}" class="edit">{$this->lang->text('edit')}</a>
 					</div><!-- END SECONDARY -->
 HTML;
 		}
@@ -37,25 +33,14 @@ HTML;
 		$html .= <<<HTML
 
 					<article class="location-and-time">
-<<<<<<< HEAD
-						<p class="item-image"><a href="{$root}files/ics/{$content->icsUID}.ics" title="{$this->lang->text('download_ical_title', $content->title)}" class="calendar-icon">
+						<p class="item-image"><a href="/files/ics/{$content->icsUID}.ics" title="{$this->lang->text('download_ical_title', $content->title)}" class="calendar-icon">
 							<span class="month">{$content->month}</span> 
 							<span class="day">{$content->day}</span> 
-							<span class="download"><img src="{$root}images/site/icon-download.png" alt="{$this->lang->text('download_ical_file')}"/>iCal</span>
+							<span class="download"><img src="/images/site/icon-download.png" alt="{$this->lang->text('download_ical_file')}"/>iCal</span>
 						</a></p>
 						<h2>{$content->location}</h2>
 						<p>{$content->startTime} - {$content->endTime}</p>
-						<p><a href="{$root}files/ics/{$content->icsUID}.ics" title="{$this->lang->text('download_ical_title', $content->title)}">{$this->lang->text('download_ical_file')}</a></p>
-=======
-						<p class="item-image"><a href="/files/ics/wddsocial.{$content->icsUID}.ics" title="Download {$content->title} iCal File" class="calendar-icon">
-							<span class="month">{$content->month}</span> 
-							<span class="day">{$content->day}</span> 
-							<span class="download"><img src="/images/site/icon-download.png" alt="Download iCal File"/>iCal</span>
-						</a></p>
-						<h2>{$content->location}</h2>
-						<p>{$content->startTime} - {$content->endTime}</p>
-						<p><a href="/files/ics/wddsocial.{$content->icsUID}.ics" title="Download {$content->title} iCal File">Download iCal File</a></p>
->>>>>>> cbf38e17b883108573d9660dcd89dc19f83fc205
+						<p><a href="/files/ics/{$content->icsUID}.ics" title="{$this->lang->text('download_ical_title', $content->title)}">{$this->lang->text('download_ical_file')}</a></p>
 					</article><!-- END {$content->title} -->
 HTML;
 		return $html;
