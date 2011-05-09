6efbea25623f60723124e834d4db30967a4219da
Anthony Colangelo <me@acolangelo.com>
2011-05-08 21:59:29 -0400

    Updated Video and Link inputs to include less by default.

--- framework5/wddsocial/view/form/pieces/LinkInputs.php
+++ framework5/wddsocial/view/form/pieces/LinkInputs.php
@@ -14,14 +14,20 @@ class LinkInputs implements \Framework5\IView {
 
 						<h1 id="links">Links</h1>
 HTML;
-		for ($i = 1; $i < 4; $i++) {
+		for ($i = 1; $i < 2; $i++) {
+			if ($i == 1) {
+				$linkNumber = '';
+			}
+			else {
+				$linkNumber = " $i";
+			}
 			$html .= <<<HTML
 
 						<fieldset>
-							<label for="link-title$i">Link $i Title</label>
+							<label for="link-title$i">Link$linkNumber Title</label>
 							<input type="text" name="link-titles[]" id="link-title$i" />
 							
-							<label for="link-url$i">Link $i URL</label>
+							<label for="link-url$i">Link$linkNumber URL</label>
 							<input type="text" name="link-urls[]" id="link-url$i" placeholder="example.com" />
 						</fieldset>
 HTML;
