c329d68f74bbf5e46666c0c2deaee7e1f4d9e4a7
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-09 06:30:47 -0400

    Moved Exception class into controller directory. Seems ridiculous to have one file in its own directory.

--- framework5/autoload.php
+++ framework5/autoload.php
@@ -31,7 +31,7 @@ call_user_func(function() {
 	# framework core files
 	require PATH_CORE.'controller/Controller.php'; # controller base
 	require PATH_CORE.'controller/StaticController.php'; # static controller base
-	require PATH_CORE.'exception/Exception.php'; # custom exception
+	require PATH_CORE.'controller/Exception.php'; # custom exception
 	require PATH_CORE.'config/Settings.php'; # configuration file
 	require PATH_CORE.'config/Router.php'; # primary routing file
 	require PATH_CORE.'controller/PackageManager.php'; # 
