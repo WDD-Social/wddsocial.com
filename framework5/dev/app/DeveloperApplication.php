e2f5c3c222cc3b7612d91985f0edac9ce0fbfa0c
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-11 07:38:59 -0400

    Disabled debug logging in Dev App

--- framework5/dev/app/DeveloperApplication.php
+++ framework5/dev/app/DeveloperApplication.php
@@ -17,6 +17,11 @@ final class DeveloperApplication extends ApplicationBase implements IApplication
 	
 	public function execute() {
 		
+		# disable logging
+		Settings::$log_debug = false;
+		Settings::$log_execution = false;
+		Settings::$log_exception = false;
+		
 		# resolve request to a page controller
 		import('dev.config.Framework5\Dev\Router');
 		$package = Dev\Router::resolve(Request::segment(1));
