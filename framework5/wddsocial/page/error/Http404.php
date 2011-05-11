1e31abdc3b565ef3fcc6c2a06a8dd11c0ce177a4
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-11 05:37:53 -0400

    Fixed 404 page invalid template package, now using alias

--- framework5/wddsocial/page/error/Http404.php
+++ framework5/wddsocial/page/error/Http404.php
@@ -12,15 +12,13 @@ class Http404 implements \Framework5\IExecutable {
 	public function execute() {
 		
 		# page header
-		echo render('wddsocial.view.WDDSocial\TemplateView', 
-			array('section' => 'top', 'title' => '404: Page not found'));
+		echo render(':template', array('section' => 'top', 'title' => '404: Page not found'));
 			
 		echo render(':section', array('section' => 'begin_content'));
 		echo render('wddsocial.view.page.WDDSocial\Http404View');
 		echo render(':section', array('section' => 'end_content'));
 		
 		# page footer
-		echo render('wddsocial.view.WDDSocial\TemplateView', 
-			array('section' => 'bottom'));
+		echo render(':template', array('section' => 'bottom'));
 	}
 }
