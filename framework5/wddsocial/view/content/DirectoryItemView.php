e58f62a7a36c3ec09d1b22fa255ce86cbcb672c4
Anthony Colangelo <me@acolangelo.com>
2011-05-11 12:07:50 -0400

    Refactored DirectoryItemView to include only two functions, content_display (for projects, articles, events, and jobs) and course_display (for courses).

--- framework5/wddsocial/view/content/DirectoryItemView.php
+++ framework5/wddsocial/view/content/DirectoryItemView.php
@@ -13,16 +13,16 @@ class DirectoryItemView implements \Framework5\IView {
 		switch ($options['type']) {
 		
 			case 'project':
-				return $this->project_display($options['content']);
+				return $this->content_display($options['content']);
 		
 			case 'article':
-				return $this->article_display($options['content']);
+				return $this->content_display($options['content']);
 		
 			case 'event':
-				return $this->event_display($options['content']);
+				return $this->content_display($options['content']);
 		
 			case 'job':
-				return $this->job_display($options['content']);
+				return $this->content_display($options['content']);
 		
 			case 'course':
 				return $this->course_display($options['content']);
@@ -38,26 +38,29 @@ class DirectoryItemView implements \Framework5\IView {
 	* Creates a project listing
 	*/
 	
-	private function project_display($project){
-		$userDisplayName = NaturalLanguage::display_name($project->userID,"{$project->userFirstName} {$project->userLastName}");
-		$leadImage = $project->images[0];
-		$projectAvatar = (file_exists("images/uploads/{$leadImage->file}_medium.jpg"))?"/images/uploads/{$leadImage->file}_medium.jpg":"/images/site/job-default_medium.jpg";
+	private function content_display($content){
+		$userDisplayName = NaturalLanguage::display_name($content->userID,"{$content->userFirstName} {$content->userLastName}");
 		
-		if (count($project->team) > 1) {
-			$members = array();
-			foreach ($project->team as $member) {
-				$memberDisplayName = NaturalLanguage::display_name($member->id,"{$member->firstName} {$member->lastName}");
-				$memberVerbage = NaturalLanguage::view_profile($member->id,"{$member->firstName} {$member->lastName}");
-				array_push($members,"<strong><a href=\"/user/{$member->vanityURL}\" title=\"$memberVerbage\">$memberDisplayName</a></strong>");
-			}
-			$membersText = NaturalLanguage::comma_list($members);	
+		switch ($content->type) {
+			case 'project':
+				$leadImage = $content->images[0];
+				$contentAvatar = (file_exists("images/uploads/{$leadImage->file}_medium.jpg"))?"/images/uploads/{$leadImage->file}_medium.jpg":"/images/site/job-default_medium.jpg";
+				break;
+			case 'article':
+				$leadImage = $content->images[0];
+				if (file_exists("images/uploads/{$leadImage->file}_medium.jpg")) {
+					$contentAvatar = "/images/uploads/{$leadImage->file}_medium.jpg";
+				}
+				else if (file_exists("images/avatars/{$content->userAvatar}_medium.jpg")) {
+					$contentAvatar = "/images/avatars/{$content->userAvatar}_medium.jpg";
+				}
+				else {
+					$contentAvatar = "/images/site/job-default_medium.jpg";
+				}
+				break;
 		}
-		else {
-			$member = $project->team[0];
-			$memberDisplayName = NaturalLanguage::display_name($member->id,"{$member->firstName} {$member->lastName}");
-			$memberVerbage = NaturalLanguage::view_profile($member->id,"{$member->firstName} {$member->lastName}");
-			$membersText = "<strong><a href=\"/user/{$member->vanityURL}\" title=\"$memberVerbage\">$memberDisplayName</a></strong>";
-		}
+		
+		$membersText = $this->format_team_string($content->userID, $content->team, 'By');
 		
 		
 		$html = <<<HTML
@@ -66,47 +69,102 @@ class DirectoryItemView implements \Framework5\IView {
 						<div class="secondary">
 HTML;
 		# Determines what type of secondary controls to present (Flag or Edit/Delete)
-		if(UserSession::is_current($project->userID)){
+		if(UserSession::is_current($content->userID)){
 			$html .= <<<HTML
 
-							<a href="/" title="Edit &ldquo;{$project->title}&rdquo;" class="edit">Edit</a>
-							<a href="/" title="Delete &ldquo;{$project->title}&rdquo;" class="delete">Delete</a>
+							<a href="/" title="Edit &ldquo;{$content->title}&rdquo;" class="edit">Edit</a>
+							<a href="/" title="Delete &ldquo;{$content->title}&rdquo;" class="delete">Delete</a>
 HTML;
-		}else if(UserValidator::is_project_owner($project->id)){
+		}else if(UserValidator::is_project_owner($content->id)){
 			$html .= <<<HTML
 
-							<a href="/" title="Edit &ldquo;{$project->title}&rdquo;" class="edit">Edit</a>
+							<a href="/" title="Edit &ldquo;{$content->title}&rdquo;" class="edit">Edit</a>
 HTML;
 		}else if(UserSession::is_authorized()){
 			$html .= <<<HTML
 
-							<a href="/" title="Flag &ldquo;{$project->title}&rdquo;" class="flag">Flag</a>
+							<a href="/" title="Flag &ldquo;{$content->title}&rdquo;" class="flag">Flag</a>
 HTML;
 		}	
 		$html .= <<<HTML
 
 						</div><!-- END SECONDARY -->
 						
-						<p class="item-image"><a href="/project/{$project->vanityURL}" title="{$project->title}"><img src="$projectAvatar" alt="{$project->title}"/></a></p>
-						<h2><a href="/project/{$project->vanityURL}" title="{$project->title}">{$project->title}</a></h2>
-						<p class="team">By $membersText</p>
-						<p>{$project->description}</p>
+						<p class="item-image"><a href="/{$content->type}/{$content->vanityURL}" title="{$content->title}"><img src="$contentAvatar" alt="{$content->title}"/></a></p>
+						<h2><a href="/{$content->type}/{$content->vanityURL}" title="{$content->title}">{$content->title}</a></h2>
+						<p class="team">$membersText</p>
+						<p>{$content->description}</p>
 HTML;
 		$html .= <<<HTML
 
-						<p class="comments"><a href="/project/{$project->vanityURL}#comments" title="{$project->title} | Comments">{$project->comments} comments</a> <span class="hidden">|</span> <span class="time">{$project->date}</span></p>
+						<p class="comments"><a href="/{$content->type}/{$content->vanityURL}#comments" title="{$content->title} | Comments">{$content->comments} comments</a> <span class="hidden">|</span> <span class="time">{$content->date}</span></p>
 HTML;
 		
 		# Build categories
 		$categoryLinks = array();
-		foreach($project->categories as $category){
+		foreach($content->categories as $category){
 			array_push($categoryLinks,"<a href=\"/search/$category\" title=\"Categories | $category\">$category</a>");
 		}
 		$categoryLinks = implode(' ',$categoryLinks);
 		$html .= <<<HTML
 						<p class="tags">$categoryLinks</p>
-					</article><!-- END {$project->title} -->
+					</article><!-- END {$content->title} -->
 HTML;
 		return $html;
+	}
+	
+	
+	/**
+	* Creates and formats the team string for display
+	*/
+	
+	private function format_team_string($ownerID, $team, $introWord, $removeOwner = false){
+		# Remove user who posted content from team (for intro sentence), and put current user at front of array
+		$cleanTeam = $team;
+		foreach($cleanTeam as $member){
+			if($member->id == $ownerID and $removeOwner){
+				$key = array_search($member, $cleanTeam);
+				unset($cleanTeam[$key]);
+			}else if(UserSession::is_current($member->id)){
+				$key = array_search($member, $cleanTeam);
+				$currentUser = $cleanTeam[$key];
+				unset($cleanTeam[$key]);
+				array_unshift($cleanTeam,$currentUser);
+			}
+		}
+		$cleanTeam = array_values($cleanTeam);
+		
+		# Create team string
+		if(count($cleanTeam) > 0){
+			$teamIntro = "$introWord ";
+			$teamString = array();
+			
+			# Creates string according to how many team members there are for this piece of content
+			if(count($cleanTeam) == 1){
+				$userVerbage = NaturalLanguage::view_profile($cleanTeam[0]->id,"{$cleanTeam[0]->firstName} {$cleanTeam[0]->lastName}");
+				$userDisplayName = NaturalLanguage::display_name($cleanTeam[0]->id,"{$cleanTeam[0]->firstName} {$cleanTeam[0]->lastName}");
+				$teamIntro .= "<strong><a href=\"/user/{$cleanTeam[0]->vanityURL}\" title=\"$userVerbage\">$userDisplayName</a></strong>";
+			}else if(count($cleanTeam) == 2){
+				foreach($cleanTeam as $member){
+					$userVerbage = NaturalLanguage::view_profile($member->id,"{$member->firstName} {$member->lastName}");
+					$userDisplayName = NaturalLanguage::display_name($member->id,"{$member->firstName} {$member->lastName}");
+					array_push($teamString, "<strong><a href=\"/user/{$member->vanityURL}\" title=\"$userVerbage\">$userDisplayName</a></strong>");
+				}
+				$teamString = implode(' and ',$teamString);
+				$teamIntro .= $teamString;
+			}else{
+				$strings = array();
+				foreach($cleanTeam as $member){
+					$userVerbage = NaturalLanguage::view_profile($member->id,"{$member->firstName} {$member->lastName}");
+					$userDisplayName = NaturalLanguage::display_name($member->id,"{$member->firstName} {$member->lastName}");
+					array_push($strings,"<strong><a href=\"/user/{$member->vanityURL}\" title=\"$userVerbage\">$userDisplayName</a></strong>");
+				}
+				$teamIntro .= NaturalLanguage::comma_list($strings);
+			}
+		}else{
+			$teamIntro = "";
+		}
+		
+		return $teamIntro;
 	}
 }
