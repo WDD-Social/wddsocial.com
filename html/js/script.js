8f61beff0fa3f05bc6a5d041aa5f173a5acc5a81
Anthony Colangelo <me@acolangelo.com>
2011-05-12 01:46:08 -0400

    Added testing AJAX to script.js

--- html/js/script.js
+++ html/js/script.js
@@ -7,6 +7,13 @@ $(function() {
 		minHeight: $('.dashboard #share').outerHeight(true) + $('.dashboard #events').outerHeight(true) + $('.dashboard #jobs').outerHeight(true)
 	});
 	
+	/* SETUP
+	****************************************************************** */
+	
+	$.ajaxSetup({
+		type: "POST"
+	});
+	
 	$('.filters a').live('click',function(){
 		$(this).parent().find('a.current').removeClass('current');
 		$(this).addClass('current');
@@ -18,6 +25,23 @@ $(function() {
 				.find('article.'+class).slideDown(500,'easeInOutQuad').parent()
 				.find('article:not(article.'+class+')').slideUp(500,'easeInOutQuad');
 		}
+		return false;
+	});
+	
+	var i = 0;
+	$('#load-more').live('click',function(){
+		$.ajax({
+			url: '/ajax/latest',
+			dataType: 'html',
+			data: {
+				start: i,
+				limit: 1
+			},
+			success: function(response){
+				$('#latest').append(response);
+			}
+		});
+		i++;
 		return false;
 	});
 });
