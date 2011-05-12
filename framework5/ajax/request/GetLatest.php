4b7825b8559b198cc064088f494c57f192848714
Anthony Colangelo <me@acolangelo.com>
2011-05-12 15:15:17 -0400

    Updated Ajax\GetLatest to be clearer.

--- framework5/ajax/request/GetLatest.php
+++ framework5/ajax/request/GetLatest.php
@@ -20,37 +20,52 @@ class GetLatest implements \Framework5\IExecutable {
 		$this->db = instance(':db');
 		$this->sql = instance(':sel-sql');
 		
-		if (!isset($_POST['start'])) {
-			$start = 0;
-		}
-		else {
-			$start = $_POST['start'];
-		}
-		
-		if (!isset($_POST['limit'])) {
-			$limit = 10;
-		}
-		else {
-			$limit = $_POST['limit'];
-		}
-		
 		switch ($_POST['query']) {
 			case 'user':
-				$query = $this->db->prepare($this->sql->getUserLatest . " LIMIT $start, $limit");
-				$query->execute(array('id' => $_POST['userID']));
+				echo $this->getUserLatest($_POST['start'],$_POST['limit'],$_POST['extra']['userID']);
 				break;
 			default:
-				$query = $this->db->prepare($this->sql->getLatest . " LIMIT $start, $limit");
-				$query->execute();
+				echo $this->getAllLatest($_POST['start'],$_POST['limit']);
 				break;
 		}
+	}
+	
+	private function getAllLatest($start, $limit){
+		if (!isset($start)) {
+			$start = 0;
+		}
 		
+		if (!isset($limit)) {
+			$limit = 10;
+		}
+		
+		$query = $this->db->prepare($this->sql->getLatest . " LIMIT $start, $limit");
+		$query->execute();
 		$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\DisplayVO');
 		
-		# display section items
+		$response = '';
 		while ($item = $query->fetch()) {
-			echo render('wddsocial.view.content.WDDSocial\MediumDisplayView', 
-				array('type' => $item->type,'content' => $item));
+			$response .= render('wddsocial.view.content.WDDSocial\MediumDisplayView', array('type' => $item->type,'content' => $item));
 		}
+		return $response;
+	}
+	
+	private function getUserLatest($start, $limit, $userID){
+		if (!isset($start)) {
+			$start = 0;
+		}
+		
+		if (!isset($limit)) {
+			$limit = 10;
+		}
+		
+		$query = $this->db->prepare($this->sql->getUserLatest . " LIMIT $start, $limit");
+		$query->execute(array('id' => $userID));
+		$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\DisplayVO');
+		
+		while ($item = $query->fetch()) {
+			$response .= render('wddsocial.view.content.WDDSocial\MediumDisplayView', array('type' => $item->type,'content' => $item));
+		}
+		return $response;
 	}
 }
