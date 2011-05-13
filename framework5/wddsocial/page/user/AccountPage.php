d8d2740e3dec7828e857bfc5ac883236ec3cc8d5
Anthony Colangelo <me@acolangelo.com>
2011-05-13 03:32:02 -0400

    Created AccountPage

--- framework5/wddsocial/page/user/AccountPage.php
+++ framework5/wddsocial/page/user/AccountPage.php
@@ -9,7 +9,25 @@ namespace WDDSocial;
 
 class AccountPage implements \Framework5\IExecutable {
 	
+	private $user;
+	
+	public function __construct() {
+		$this->lang = new \Framework5\Lang('wddsocial.lang.page.global.UserPageLang');
+		$this->db = instance(':db');
+		$this->sql = instance(':sel-sql');
+	}
+	
+	
+	
 	public function execute() {
+		
+		# get the request user
+		$this->user = $this->get_user($_SESSION['user']->id);
+		
+		if (isset($_POST['submit'])) {
+			$this->_process_form();
+			return true;
+		}
 		
 		# display site header
 		echo render(':template', array('section' => 'top', 'title' => 'My Account'));
@@ -18,18 +36,56 @@ class AccountPage implements \Framework5\IExecutable {
 		echo render(':section', array('section' => 'begin_content'));
 		
 		# display account form
-		echo render('wddsocial.view.form.WDDSocial\AccountView', array('user' => $_SESSION['user']));
+		echo render('wddsocial.view.form.WDDSocial\AccountView', array('user' => $this->user, 'error' => $errorMessage));
 			
 		# end content section
 		echo render(':section', array('section' => 'end_content'));
 		
 		# display site footer
 		echo render(':template', array('section' => 'bottom'));
+		
+		$this->_dump_user();
 	}
 	
 	
 	
 	private function _process_form(){
+		echo "<h1>POST:</h1>";
 		
+		echo "<pre>";
+		print_r($_POST);
+		echo "</pre>";
+		
+		echo "<h1>USER:</h1>";
+		
+		echo "<pre>";
+		print_r($this->user);
+		echo "</pre>";
+	}
+	
+	
+	
+	private function _dump_user(){
+		echo "<pre>";
+		print_r($this->user);
+		echo "</pre>";
+	}
+	
+	
+	
+	/**
+	* Gets the user and data
+	*/
+	
+	private function get_user($id){
+		
+		import('wddsocial.model.WDDSocial\UserVO');
+		
+		# query
+		$data = array('id' => $id);
+		$query = $this->db->prepare($this->sql->getUserByID);
+		$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\UserVO');
+		$query->execute($data);
+		return $query->fetch();
 	}
 }
