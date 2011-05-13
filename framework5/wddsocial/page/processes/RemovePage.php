d444426b5023685e88e6c5bbf12697a9f87c1392
Anthony Colangelo <me@acolangelo.com>
2011-05-13 00:02:37 -0400

    Set up RemovePage

--- framework5/wddsocial/page/processes/RemovePage.php
+++ framework5/wddsocial/page/processes/RemovePage.php
@@ -10,5 +10,28 @@ namespace WDDSocial;
 class RemovePage implements \Framework5\IExecutable {
 	public function execute() {
 		UserSession::protect();
+		$types = array('project','article','event','job','user');
+		$type = \Framework5\Request::segment(1);
+		$vanityURL = \Framework5\Request::segment(2);
+		if (!in_array($type, $types) or !isset($vanityURL))
+			redirect('/');
+		
+		switch ($type) {
+			case 'project':
+				
+				break;
+			case 'article':
+				
+				break;
+			case 'event':
+				
+				break;
+			case 'job':
+				
+				break;
+			case 'user':
+				
+				break;
+		}
 	}
 }
