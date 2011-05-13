28e952c8487028415af380e69d822e1112fa1c37
Anthony Colangelo <me@acolangelo.com>
2011-05-13 03:31:27 -0400

    Created AccountView

--- framework5/wddsocial/view/form/AccountView.php
+++ framework5/wddsocial/view/form/AccountView.php
@@ -7,79 +7,114 @@ namespace WDDSocial;
 * @author Anthony Colangelo (me@acolangelo.com)
 */
 
-class AccountView implements \Framework5\IView {		
-	
+class AccountView implements \Framework5\IView {
 	public function render($options = null) {
-		/* return */ <<<HTML
+		$user = $options['user'];
+		$html .= <<<HTML
 
 					<form action="/account" method="post" enctype="multipart/form-data">
-					<h1>Basic</h1>
-					<p class="error"><strong>{$options['error']}</strong></p>
-					<fieldset>
-						<label for="first-name">First Name</label>
-						<input type="text" name="first-name" id="first-name" />
-					</fieldset>
-					<fieldset>
-						<label for="last-name">Last Name</label>
-						<input type="text" name="last-name" id="last-name" />
-					</fieldset>
-					<fieldset>
-						<label for="email">Email</label>
-						<input type="email" name="email" id="email" />
-					</fieldset>
-					<fieldset>
-						<label for="full-sail-email">Full Sail Email</label>
-						<input type="email" name="full-sail-email" id="full-sail-email" />
-						<small>Used for account verification</small>
-					</fieldset>
-					
-					<h1>Change Password</h1>
-					<fieldset>
-						<label for="old-password">Old Password</label>
-						<input type="password" name="old-password" id="old-password" />
-					</fieldset>
-					<fieldset>
-						<label for="new-password">New Password</label>
-						<input type="password" name="new-password" id="new-password" />
-						<small>6 or more characters</small>
-					</fieldset>
-					
-					<h1>Background</h1>
-					<fieldset class="radio">
-						<label>I am a...</label>
-						<div>
-							<input type="radio" id="student" name="user-type" value="student" />
-							<label for="student">Student</label>
-							
-							<input type="radio" id="teacher" name="user-type" value="teacher" />
-							<label for="teacher">Teacher</label>
-							
-							<input type="radio" id="alum" name="user-type" value="alum" />
-							<label for="alum">Alum</label>
-						</div>
-					</fieldset>
-					<fieldset>
-						<label for="avatar">Avatar</label>
-						<input type="file" name="avatar" id="avatar" />
-					</fieldset>
-					<fieldset>
-						<label for="hometown">Hometown</label>
-						<input type="text" name="hometown" id="hometown" />
-					</fieldset>
-					<fieldset>
-						<label for="birthday">Birthday</label>
-						<input type="text" name="birthday" id="birthday" />
-					</fieldset>
-					<fieldset>
-						<label for="bio">Bio</label>
-						<textarea id="bio"></textarea>
-						<small><span class="count">255</span> characters left</small>
-					</fieldset>
-					
-					<p class="helper-link"><a href="/remove/account" title="Delete your WDD Social Account">Delete Account</a></p>
-					<input type="hidden" name="process" value="account" />
-					<input type="submit" value="Save" />
-				</form>
+						<h1>Basics</h1>
+						<p class="error"><strong>{$options['error']}</strong></p>
+						<fieldset>
+							<label for="first-name">First Name</label>
+							<input type="text" name="first-name" id="first-name" value="{$user->firstName}" />
+						</fieldset>
+						<fieldset>
+							<label for="last-name">Last Name</label>
+							<input type="text" name="last-name" id="last-name" value="{$user->lastName}" />
+						</fieldset>
+						<fieldset>
+							<label for="email">Email</label>
+							<input type="email" name="email" id="email" value="{$user->email}" />
+						</fieldset>
+						<fieldset>
+							<label for="full-sail-email">Full Sail Email</label>
+							<input type="email" name="full-sail-email" id="full-sail-email" value="{$user->fullsailEmail}" />
+							<small>Used for account verification</small>
+						</fieldset>
+						<fieldset>
+							<label for="vanityURL">Custom Vanity URL</label>
+							<input type="text" name="vanityURL" id="vanityURL" value="{$user->vanityURL}" />
+							<small>wddsocial.com/user/<strong>{$user->vanityURL}</strong></small>
+						</fieldset>
+						<input type="submit" name="submit" value="Save" />
+						
+						<h1>Change Password</h1>
+						<fieldset>
+							<label for="old-password">Old Password</label>
+							<input type="password" name="old-password" id="old-password" />
+						</fieldset>
+						<fieldset>
+							<label for="new-password">New Password</label>
+							<input type="password" name="new-password" id="new-password" />
+							<small>6 or more characters, please</small>
+						</fieldset>
+						<input type="submit" name="submit" value="Save" />
+						
+						<h1>More Information</h1>
 HTML;
+		$html .= <<<HTML
+
+						<fieldset>
+							<label for="avatar">New Avatar</label>
+							<input type="file" name="avatar" id="avatar" />
+						</fieldset>
+						<fieldset>
+							<label for="hometown">Hometown</label>
+							<input type="text" name="hometown" id="hometown" value="{$user->hometown}" />
+						</fieldset>
+						<fieldset>
+							<label for="birthday">Birthday</label>
+							<input type="text" name="birthday" id="birthday" value="{$user->birthday}" />
+						</fieldset>
+						<fieldset>
+							<label for="bio">Bio</label>
+							<textarea id="bio" name="bio">{$user->bio}</textarea>
+							<small>Describe yourself in <span class="count">255</span> characters or less</small>
+						</fieldset>
+HTML;
+		$html .= render('wddsocial.view.form.pieces.WDDSocial\UserTypeSelector', array('typeID' => $user->typeID, 'required' => false));
+		$html .= render('wddsocial.view.form.pieces.WDDSocial\UserDetailInputs', $user->extra);
+		
+		$html .= <<<HTML
+
+						<input type="submit" name="submit" value="Save" />
+
+						<h1>Contact</h1>
+						<fieldset>
+							<label for="website">Website</label>
+							<input type="text" name="website" id="website" placeholder="example.com" value="{$user->contact['website']}" />
+						</fieldset>
+						<fieldset>
+							<label for="twitter">Twitter</label>
+							<input type="text" name="twitter" id="twitter" placeholder="username" value="{$user->contact['twitter']}" />
+							<small>@username</small>
+						</fieldset>
+						<fieldset>
+							<label for="facebook">Facebook</label>
+							<input type="text" name="facebook" id="facebook" placeholder="username" value="{$user->contact['facebook']}" />
+							<small>facebook.com/username</small>
+						</fieldset>
+						<fieldset>
+							<label for="github">Github</label>
+							<input type="text" name="github" id="github" placeholder="username" value="{$user->contact['github']}" />
+							<small>github.com/username</small>
+						</fieldset>
+						<fieldset>
+							<label for="dribbble">Dribbble</label>
+							<input type="text" name="dribbble" id="dribbble" placeholder="username" value="{$user->contact['dribbble']}" />
+							<small>dribbble.com/username</small>
+						</fieldset>
+						<fieldset>
+							<label for="forrst">Forrst</label>
+							<input type="text" name="forrst" id="forrst" placeholder="username" value="{$user->contact['forrst']}" />
+							<small>forrst.com/people/username</small>
+						</fieldset>
+						
+						<p class="helper-link"><a href="/remove/user/{$user->vanityURL}" title="Delete your WDD Social Account">Delete Account</a></p>
+						<input type="submit" name="submit" value="Save" />
+					</form>
+HTML;
+		return $html;
 	}
 }
