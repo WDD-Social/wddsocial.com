e38a27ee13e8ff6647567bcab5fd70577ab3e906
Anthony Colangelo <me@acolangelo.com>
2011-05-15 01:33:49 -0400

    Users now must have edit access to projects/articles/events/jobs to edit them

--- framework5/wddsocial/page/form/EditPage.php
+++ framework5/wddsocial/page/form/EditPage.php
@@ -48,6 +48,27 @@ class EditPage implements \Framework5\IExecutable {
 		if ($query->rowCount() > 0) {
 			$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\ContentVO');
 			$content = $query->fetch();
+			switch ($type) {
+				case 'project':
+					$query = $this->db->prepare($this->val->isUserProjectOwner);
+					$query->execute(array('userID' => UserSession::userid(), 'projectID' => $content->id));
+					break;
+				case 'article':
+					$query = $this->db->prepare($this->val->isUserArticleOwner);
+					$query->execute(array('userID' => UserSession::userid(), 'articleID' => $content->id));
+					break;
+				case 'event':
+					$query = $this->db->prepare($this->val->isUserEventOwner);
+					$query->execute(array('userID' => UserSession::userid(), 'eventID' => $content->id));
+					break;
+				case 'job':
+					$query = $this->db->prepare($this->val->isUserJobOwner);
+					$query->execute(array('userID' => UserSession::userid(), 'jobID' => $content->id));
+					break;
+			}
+			if ($query->rowCount() == 0) {
+				redirect('/');
+			}
 		}
 		else {
 			redirect('/');
