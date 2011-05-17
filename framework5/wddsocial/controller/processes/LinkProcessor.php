def84ec2989b7f613c558149ba0c788d18b1457f
Anthony Colangelo <me@acolangelo.com>
2011-05-16 21:27:56 -0400

    Completed LinkProcessor::update_links

--- framework5/wddsocial/controller/processes/LinkProcessor.php
+++ framework5/wddsocial/controller/processes/LinkProcessor.php
@@ -20,20 +20,7 @@ class LinkProcessor {
 			if ($link != '') {
 				$i = array_search($link, $links);
 				$linkTitle = ($titles[$i] != '')?$titles[$i]:$link;
-				$data = array('link' => $link, 'title' => $linkTitle);
-				$query = $db->prepare($val_sql->checkIfLinkExists);
-				$query->execute($data);
-				$query->setFetchMode(\PDO::FETCH_OBJ);
-				$result = $query->fetch();
-				
-				if ($query->rowCount() > 0) {
-					$linkID = $result->id;
-				}
-				else {
-					$query = $db->prepare($admin_sql->addLink);
-					$query->execute($data);
-					$linkID = $db->lastInsertID();	
-				}
+				$linkID = static::get_linkID($link, $linkTitle);
 				
 				switch ($contentType) {
 					case 'project':
@@ -58,6 +45,118 @@ class LinkProcessor {
 						break;
 				}
 			}
+		}
+	}
+	
+	
+	
+	public static function update_links($currentLinks, $newLinks,  $currentTitles, $newTitles, $contentID, $type){
+		$db = instance(':db');
+		$sql = instance(':admin-sql');
+		
+		$currentLinkIDs = array();
+		$newLinkIDs = array();
+		
+		foreach ($currentLinks as $currentLink) {
+			$i = array_search($currentLink, $currentLinks);
+			$linkTitle = ($currentTitles[$i] != '')?$currentTitles[$i]:$currentLink;
+			$linkID = static::get_linkID($currentLink, $linkTitle);
+			array_push($currentLinkIDs,$linkID);
+		}
+		
+		foreach ($newLinks as $newLink) {
+			$i = array_search($newLink, $newLinks);
+			$linkTitle = ($newTitles[$i] != '')?$newTitles[$i]:$newLink;
+			$linkID = static::get_linkID($newLink, $linkTitle);
+			array_push($newLinkIDs,$linkID);
+		}
+		
+		foreach ($newLinkIDs as $newLinkID) {
+			if (in_array($newLinkID, $currentLinkIDs)) {
+				unset($currentLinkIDs[array_search($newLinkID, $currentLinkIDs)]);
+				unset($newLinkIDs[array_search($newLinkID, $newLinkIDs)]);
+			}
+		}
+		
+		if (count($currentLinkIDs) > 0) {
+			switch ($type) {
+				case 'project':
+					foreach ($currentLinkIDs as $currentLinkID) {
+						$query = $db->prepare($sql->deleteProjectLink);
+						$query->execute(array('projectID' => $contentID, 'linkID' => $currentLinkID));
+					}
+					break;
+				case 'article':
+					foreach ($currentLinkIDs as $currentLinkID) {
+						$query = $db->prepare($sql->deleteArticleLink);
+						$query->execute(array('articleID' => $contentID, 'linkID' => $currentLinkID));
+					}
+					break;
+				case 'event':
+					foreach ($currentLinkIDs as $currentLinkID) {
+						$query = $db->prepare($sql->deleteEventLink);
+						$query->execute(array('eventID' => $contentID, 'linkID' => $currentLinkID));
+					}
+					break;
+				case 'job':
+					foreach ($currentLinkIDs as $currentLinkID) {
+						$query = $db->prepare($sql->deleteJobLink);
+						$query->execute(array('jobID' => $contentID, 'linkID' => $currentLinkID));
+					}
+					break;
+			}
+		}
+		
+		if (count($newLinks) > 0) {
+			switch ($type) {
+				case 'project':
+					foreach ($newLinkIDs as $newLinkID) {
+						$query = $db->prepare($sql->addProjectLink);
+						$query->execute(array('projectID' => $contentID, 'linkID' => $newLinkID));
+					}
+					break;
+				case 'article':
+					foreach ($newLinkIDs as $newLinkID) {
+						$query = $db->prepare($sql->addArticleLink);
+						$query->execute(array('articleID' => $contentID, 'linkID' => $newLinkID));
+					}
+					break;
+				case 'event':
+					foreach ($newLinkIDs as $newLinkID) {
+						$query = $db->prepare($sql->addEventLink);
+						$query->execute(array('eventID' => $contentID, 'linkID' => $newLinkID));
+					}
+					break;
+				case 'job':
+					foreach ($newLinkIDs as $newLinkID) {
+						$query = $db->prepare($sql->addJobLink);
+						$query->execute(array('jobID' => $contentID, 'linkID' => $newLinkID));
+					}
+					break;
+			}
+		}
+	}
+	
+	
+	
+	public static function get_linkID($link, $linkTitle){
+		$db = instance(':db');
+		$val_sql = instance(':val-sql');
+		$admin_sql = instance(':admin-sql');
+		
+		$data = array('link' => $link, 'title' => $linkTitle);
+		$query = $db->prepare($val_sql->checkIfLinkExists);
+		$query->execute($data);
+		$query->setFetchMode(\PDO::FETCH_OBJ);
+		$result = $query->fetch();
+		
+		if ($query->rowCount() > 0) {
+			return $result->id;
+		}
+		else if($link != '') {
+			$query = $db->prepare($admin_sql->addLink);
+			$query->execute($data);
+			return $db->lastInsertID();	
 		}
 	}
 }
