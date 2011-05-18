e3e8b3bbb31ebf314a7f5e55969fa6577dd7cefa
Anthony Colangelo <me@acolangelo.com>
2011-05-17 20:06:28 -0400

    DeletePage now pulls in real ContentVO/UserVo

--- framework5/wddsocial/page/form/DeletePage.php
+++ framework5/wddsocial/page/form/DeletePage.php
@@ -24,44 +24,52 @@ class DeletePage implements \Framework5\IExecutable {
 		if (!in_array($type, $types) or !isset($vanityURL))
 			redirect('/');
 		
-		if ($type = 'user')
-			import('wddsocial.model.WDDSocial\ContentVO');
-		else
-			import('wddsocial.model.WDDSocial\UserVO');
-		
 		switch ($type) {
 			case 'project':
 				$query = $this->db->prepare($this->sel->getProjectByVanityURL);
-				$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\ContentVO');
 				break;
 			case 'article':
 				$query = $this->db->prepare($this->sel->getArticleByVanityURL);
-				$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\ContentVO');
 				break;
 			case 'event':
 				$query = $this->db->prepare($this->sel->getEventByVanityURL);
-				$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\ContentVO');
 				break;
 			case 'job':
 				$query = $this->db->prepare($this->sel->getJobByVanityURL);
-				$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\ContentVO');
 				break;
 			case 'user':
 				$query = $this->db->prepare($this->sel->getUserByVanityURL);
-				$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\UserVO');
 				break;
 			default:
 				redirect('/');
 				break;
 		}
 		
-		$typeTitle = ucfirst($content->type);
+		if ($type != 'user') {
+			import('wddsocial.model.WDDSocial\ContentVO');
+			$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\ContentVO');
+		}
+		else {
+			import('wddsocial.model.WDDSocial\UserVO');
+			$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\UserVO');
+		}
+			
+		$query->execute(array('vanityURL' => $vanityURL));
+		$content = $query->fetch();
+		if ($type == 'user') $content->title = $content->firstName . ' ' . $content->lastName;
+		$typeTitle = ucfirst($type);
 		
 		# display site header
 		echo render(':template', array('section' => 'top', 'title' => "Delete {$typeTitle} | {$content->title}"));
 		
 		# open content section
 		echo render(':section', array('section' => 'begin_content'));
+		
+		/*
+echo "<pre>";
+		print_r($content);
+		echo "</pre>";
+*/
 		
 		# end content section
 		echo render(':section', array('section' => 'end_content'));
