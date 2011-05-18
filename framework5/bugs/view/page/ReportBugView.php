4999eb326cd613064ec717a3a2ab3a8007e2c36b
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-18 01:51:26 -0400

    Changed bug submit template to the site template

--- framework5/bugs/view/page/ReportBugView.php
+++ framework5/bugs/view/page/ReportBugView.php
@@ -12,6 +12,7 @@ class ReportBugView implements \Framework5\IView {
 	public function render($error = null) {
 		return <<<HTML
 			<h1>What seems to be the problem?</h1>
+			<p>Providing us with a detailed description of any problem you encounter is a step towards a user friendly community. Thank you for your support.</p>
 			<h2>{$error}</h2>
 			<form method="post">
 				<textarea name="message" style="width: 97%;"></textarea>
