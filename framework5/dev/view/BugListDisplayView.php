9774a56b917ceeeeaed2333f3710996ab9e41075
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-18 03:26:40 -0400

    Fixed database fetch problem

--- framework5/dev/view/BugListDisplayView.php
+++ framework5/dev/view/BugListDisplayView.php
@@ -12,10 +12,9 @@ class BugListDisplayView implements \Framework5\IView {
 	public function render($bugs = null) {
 		
 		foreach ($bugs as $bug) {
-			echo "for {$bug->request_id}";
 			
 			$html .= <<<HTML
-			<a href="/dev/bug/{$bug->id}">{$bug->id}</a>
+			<a href="/dev/bug/{$bug->id}">{$bug->request_id}</a>
 
 HTML;
 		}
