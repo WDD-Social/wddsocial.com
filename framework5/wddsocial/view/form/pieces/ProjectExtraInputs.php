938d6c850b6f920e10f4a47ab4f663ce096c4937
Anthony Colangelo <me@acolangelo.com>
2011-05-19 02:19:16 -0400

    Updated Project/Event ExtraInputs for updating date hinting and values

--- framework5/wddsocial/view/form/pieces/ProjectExtraInputs.php
+++ framework5/wddsocial/view/form/pieces/ProjectExtraInputs.php
@@ -10,18 +10,19 @@ namespace WDDSocial;
 class ProjectExtraInputs implements \Framework5\IView {		
 	
 	public function render($options = null) {
+		$now = date('F, Y');
 		if (isset($options['data'])) {
-			$dateValue = ($options['data']->completeDateInput == '0000-00-00')?'':$options['data']->completeDateInput;
+			$dateValue = ($options['data']->completeDate == '')?'':$options['data']->completeDate;
 		}
 		else {
-			$dateValue = date('Y-m-d');
+			$dateValue = $now;
 		}
 		return <<<HTML
 
 						<fieldset>
 							<label for="completed-date">When was this project completed?</label>
 							<input type="text" name="completed-date" id="completed-date" value="{$dateValue}" />
-							<small>yyyy-mm-dd</small>
+							<small>Example: <strong>$now</strong></small>
 						</fieldset>
 HTML;
 	}
