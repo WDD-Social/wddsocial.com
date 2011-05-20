347111c10d1533d12624d46707b0bcebbdeca112
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-20 05:20:42 -0400

    Made awesome changes to issues

--- framework5/dev/view/issue/IssueView.php
+++ framework5/dev/view/issue/IssueView.php
@@ -16,9 +16,10 @@ class IssueView implements \Framework5\IView {
 		
 		# output
 		return <<<HTML
-			<a href="/dev/request/{$bug->request_id}">Request ID: {$bug->request_id}</a><br/>
+			<h1>Issue {$bug->id}</h1>
+			<p>Request ID: <a href="/dev/request/{$bug->request_id}">{$bug->request_id}</a></p>
 			<p>User ID: {$bug->user_id}</p>
-			<p>Message: {$bug->message}</p>
+			<p>Message:<br/>{$bug->message}</p>
 
 HTML;
 	}
--- framework5/dev/view/issue/IssuesView.php
+++ framework5/dev/view/issue/IssuesView.php
@@ -11,13 +11,31 @@ class IssuesView implements \Framework5\IView {
 	
 	public function render($bugs = null) {
 		
+		$html = <<<HTML
+
+			<table id="issues">
+				<tr>
+					<td>Issue Number</td>
+					<td>Message</td>
+				</tr>
+HTML;
+		
 		foreach ($bugs as $bug) {
+			$short_message = substr($bug->message, 0, 50);
 			
-			$html .= <<<HTML
-			<a href="/dev/issues/{$bug->id}">{$bug->request_id}</a><br/>
+			$html.= <<<HTML
+				<tr>
+					<td><a href="/dev/issues/{$bug->id}">Issue {$bug->id}</a></td>
+					<td>{$short_message}</td>
+				</tr>
 
 HTML;
 		}
+		
+		$html.= <<<HTML
+
+			</table>
+HTML;
 		
 		return $html;
 	}
