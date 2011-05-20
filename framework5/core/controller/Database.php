9f6adda05be7dfc3a52ab900de5104c7b0962b85
Anthony Colangelo <me@acolangelo.com>
2011-05-20 02:07:53 -0400

    Being stupid. Localhost

--- framework5/core/controller/Database.php
+++ framework5/core/controller/Database.php
@@ -12,7 +12,7 @@ final class Database extends \PDO {
 	public function __construct() {
 				
 		# call PDO construct
-		parent::__construct("mysql:host=root;dbname=sandbox", "root", "root");
+		parent::__construct("mysql:host=localhost;dbname=sandbox", "root", "root");
 		
 		# set default attributes
 		$this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
