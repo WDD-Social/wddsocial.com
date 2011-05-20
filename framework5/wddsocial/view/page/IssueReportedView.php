aa68cd81c8a1c8494c46e85d0bfaa73c8832dcb3
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-19 23:46:02 -0400

    Big Text

--- framework5/wddsocial/view/page/IssueReportedView.php
+++ framework5/wddsocial/view/page/IssueReportedView.php
@@ -11,7 +11,7 @@ class IssueReportedView implements \Framework5\IView {
 	
 	public function render($error = null) {
 		return <<<HTML
-			<h1>Thank you</h1>
+			<h1 class="mega">Thank you</h1>
 			<p>Your issue has been reported. A gathering of magical developer gnomes will be along shortly to tidy things up.</p>
 HTML;
 	}
