13ccc80c658d52e357f5a1c2bb890a887cccad98
Anthony Colangelo <me@acolangelo.com>
2011-05-20 15:43:29 -0400

    Began building JobsPage

--- framework5/wddsocial/page/global/JobsPage.php
+++ framework5/wddsocial/page/global/JobsPage.php
@@ -20,10 +20,15 @@ class JobsPage implements \Framework5\IExecutable {
 	
 	public function execute() {
 		
-		$page_title = $this->lang->text('page-title'); # set page title
+		# display site header
+		$page_title = 'Jobs';
+		
 		
 		
 		$content = render(':section', array('section' => 'begin_content'));
+		
+		
+		
 		$content .= render(':section', array('section' => 'end_content'));
 		
 		# display page
