6530db2e42f3cc466ac84f7cb40806fd4e8af306
Anthony Colangelo <me@acolangelo.com>
2011-05-21 16:24:34 -0400

    Created Validator.php

---
+++ framework5/wddsocial/controller/Validator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace WDDSocial;
+
+/*
+* 
+* 
+* @author: Anthony Colangelo (me@acolangelo.com)
+*/
+
+class Validator {
+	
+	/**
+	* Checks if a project has been flagged
+	*/
+	
+	public static function project_flagged($projectID){
+		
+	}
+}
