84630bf21870a24af3d1c39dc3ceb93f998b5aab
Anthony Colangelo <me@acolangelo.com>
2011-05-23 13:57:38 -0400

    Completed categories and courses autocomplete responses

--- framework5/ajax/request/Autocompleter.php
+++ framework5/ajax/request/Autocompleter.php
@@ -23,11 +23,35 @@ class Autocompleter implements \Framework5\IExecutable {
 				$response->status = (count($response->results) > 0)?true:false;
 				echo json_encode($response);
 				break;
+			case 'categories':
+				$response->results = $this->get_categories($_POST['term']);
+				$response->status = (count($response->results) > 0)?true:false;
+				echo json_encode($response);
+				break;
+			case 'courses':
+				$response->results = $this->get_courses($_POST['term']);
+				$response->status = (count($response->results) > 0)?true:false;
+				echo json_encode($response);
+				break;
 		}
 	}
 	
 	private function get_users($term){
 		$query = $this->db->prepare($this->sql->autocompleteUsers);
+		$query->execute(array('term' => "$term%"));
+		$query->setFetchMode(\PDO::FETCH_OBJ);
+		return $query->fetchAll();
+	}
+	
+	private function get_categories($term){
+		$query = $this->db->prepare($this->sql->autocompleteCategories);
+		$query->execute(array('term' => "$term%"));
+		$query->setFetchMode(\PDO::FETCH_OBJ);
+		return $query->fetchAll();
+	}
+	
+	private function get_courses($term){
+		$query = $this->db->prepare($this->sql->autocompleteCourses);
 		$query->execute(array('term' => "$term%"));
 		$query->setFetchMode(\PDO::FETCH_OBJ);
 		return $query->fetchAll();
