e68488ee72446940d35a0f3db3b106cb01632304
Anthony Colangelo <me@acolangelo.com>
2011-05-24 16:35:52 -0400

    Touched up start conversation form, added autocompleters to start conversation input. Selected conversation now stays selected

--- framework5/wddsocial/view/messages/ConversationsView.php
+++ framework5/wddsocial/view/messages/ConversationsView.php
@@ -22,15 +22,18 @@ class ConversationsView implements \Framework5\IView {
 	public function render($options = null) {
 		$html = <<<HTML
 					
-					<div class="secondary filters">
-						<a href="messages.html#all" title="{$this->lang->text('all-conversations-filter')}" class="current">{$this->lang->text('all-filter')}</a> 
-						<a href="messages.html#unread" title="{$this->lang->text('unread-conversations-filter')}">{$this->lang->text('unread-filter')}</a> 
-					</div><!-- END SECONDARY -->
+					<!--<div class="secondary filters">
+						<a href="/messages#all" title="{$this->lang->text('all-conversations-filter')}" class="current">{$this->lang->text('all-filter')}</a> 
+						<a href="/messages#unread" title="{$this->lang->text('unread-conversations-filter')}">{$this->lang->text('unread-filter')}</a> 
+					</div>-->
 					
-					<form action="" method="post">
+					<form action="/messages" method="post">
+						<p class="error"><strong>{$options['error']}</strong></p>
 						<fieldset>
 							<label for="to">{$this->lang->text('start-a-convo')}</label>
-							<input type="text" name="to" id="to" placeholder="{$this->lang->text('convo-with')}" />
+							<input type="text" name="to" id="to" class="autocompleter" data-autocomplete="users" autocomplete="off" placeholder="{$this->lang->text('convo-with')}" />
+							<input type="hidden" name="process" value="start" />
+							<small>Press <strong>Enter</strong> to begin conversation</small>
 						</fieldset>
 					</form>
 HTML;
@@ -43,7 +46,7 @@ HTML;
 		
 		# no messages
 		else {
-			$html.= "<p>{$this->lang->text('no-messages')}</p>";
+			$html.= "<p class=\"empty\">{$this->lang->text('no-messages')}</p>";
 		}
 		
 		
@@ -54,10 +57,11 @@ HTML;
 	
 	private function renderConversation($convo) {
 		$userAvatar = (file_exists("images/avatars/{$convo->userAvatar}_medium.jpg"))?"/images/avatars/{$convo->userAvatar}_medium.jpg":"/images/site/user-default_medium.jpg";
+		$currentClass = ($convo->userVanityURL == \Framework5\Request::segment(1))?'current':'';
 		$unreadClass = ($convo->unread > 0)?'unread':'';
 		$badge = ($convo->unread > 0)?"<span class=\"badge\">{$convo->unread}</span>":'';
 		return <<<HTML
-					<a href="/messages/{$convo->userVanityURL}" title="{$this->lang->text('view-convo-with', $convo->userName)}" class="{$unreadClass}">
+					<a href="/messages/{$convo->userVanityURL}" title="{$this->lang->text('view-convo-with', $convo->userName)}" class="{$unreadClass} {$currentClass}">
 						<img src="{$userAvatar}" alt="{$convo->userName}"/>
 						<h2>{$badge}{$convo->userName}</h2>
 						<p>{$convo->message}</p>
