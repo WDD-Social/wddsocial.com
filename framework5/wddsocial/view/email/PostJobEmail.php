e7628ce9e6afebcfa70130c4eaa1504364118947
Anthony Colangelo <me@acolangelo.com>
2011-05-26 13:36:55 -0400

    Un-goofed the PostJobEmail view

--- framework5/wddsocial/view/email/PostJobEmail.php
+++ framework5/wddsocial/view/email/PostJobEmail.php
@@ -16,9 +16,9 @@ class PostJobEmail implements \Framework5\IView {
 			<h2>Congratulations, your job has been posted!</h2>
 			<p>You&rsquo;ve recently posted a job opportunity on <a href="http://wddsocial.com/" title="WDD Social">WDD Social</a>. Please <strong>do not lose this email</strong> because it contains important information regarding editing and deleting your job posting.</p>
 			<h2>Edit your Job Post</h2>
-			<p><a href="http://dev.wddsocial.com/{$options['edit-link']}" title="WDD Social | Edit your Job Post">Click here to edit your job post.</a></p>
+			<p><a href="http://dev.wddsocial.com/edit/job/{$options['vanityURL']}/{$options['securityCode']}" title="WDD Social | Edit your Job Post">Click here to edit your job post.</a></p>
 			<h2>Delete your Job Post</h2>
-			<p><a href="http://dev.wddsocial.com/{$options['delete-link']}" title="WDD Social | Delete your Job Post">Click here to delete your job post.</a></p>
+			<p><a href="http://dev.wddsocial.com/delete/job/{$options['vanityURL']}/{$options['securityCode']}" title="WDD Social | Delete your Job Post">Click here to delete your job post.</a></p>
 			<p>Thank you for contributing to our vibrant community, and we hope to see you again!</p>
 			<p class="signOff">WDD Social Team</p>
 HTML;
