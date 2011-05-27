a1508c59004186b37512c28e5e081f81513edfca
Anthony Colangelo <me@acolangelo.com>
2011-05-26 21:29:44 -0400

    Fixed merge issue

--- framework5/core/controller/Database.php
+++ framework5/core/controller/Database.php
@@ -12,7 +12,7 @@ final class Database extends \PDO {
 	public function __construct() {
 				
 		# call PDO construct
-		parent::__construct("mysql:host=internal-db.s112587.gridserver.com;dbname=db112587_fw5debug", "db112587_social", "G*Uoj9F|S0i4f+tD");
+		parent::__construct("mysql:host=localhost;dbname=sandbox", "root", "root");
 		
 		# set default attributes
 		$this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
