dbff1e9c33a1455f11dded164709ff6888db5777
Anthony Colangelo <me@acolangelo.com>
2011-05-27 02:35:43 -0400

    Fixed issue in cron job

--- framework5/wddsocial/controller/cron/DeleteOldICSCron.php
+++ framework5/wddsocial/controller/cron/DeleteOldICSCron.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO("mysql:host=internal-db.s112587.gridserver.com;dbname=db112587_wddsocial", "db112587_social", "G*Uoj9F|S0i4f+tD");
+$pdo = new PDO("mysql:host=localhost;dbname=wddsocial", "root", "root");
 
 $query = $pdo->query("SELECT icsUID FROM events AS e WHERE TIMESTAMPDIFF(MINUTE,NOW(),endDateTime) < 0");
 $query->setFetchMode(PDO::FETCH_OBJ);
