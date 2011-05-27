94a48c18a39f2b8693f6d2e5e83ee80129a26d13
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-26 21:42:27 -0400

    Comments and Formatting

--- framework5/wddsocial/page/user/MessagesPage.php
+++ framework5/wddsocial/page/user/MessagesPage.php
@@ -3,13 +3,17 @@
 namespace WDDSocial;
 
 /*
-* 
+* User Messages Page
 * 
 * @author Anthony Colangelo (me@acolangelo.com)
 * @author tmatthews (tmatthewsdev@gmail.com)
 */
 
 class MessagesPage implements \Framework5\IExecutable {
+	
+	/**
+	* Include dependencies
+	*/
 	
 	public function __construct() {
 		$this->lang = new \Framework5\Lang('wddsocial.lang.page.user.MessagesPageLang');
@@ -25,27 +29,27 @@ class MessagesPage implements \Framework5\IExecutable {
 		# require authentication to access this page
 		UserSession::protect();
 		
+		# handle form submission
 		if (isset($_POST['process'])) {
 			switch ($_POST['process']) {
 				case 'start':
-					if ($_POST['to'] != '') {
+					if (!empty($_POST['to'])) {
 						$query = $this->db->prepare($this->sql->getUserByName);
 						$query->execute(array('name' => $_POST['to']));
 						if ($query->rowCount() > 0) {
 							$query->setFetchMode(\PDO::FETCH_OBJ);
 							$result = $query->fetch();
-							$vanityURL = $result->vanityURL;
-							$redirectURL = "/messages/$vanityURL";
-							redirect("$redirectURL");
+							redirect("/messages/{$result->vanityURL}");
 						}
 						else {
 							$conversationsError = 'Uh oh, that user could not be found. Please try again.';
 						}
 					}
 					break;
+				
 				case 'send':
 					$message = strip_tags($_POST['message']);
-					if ($_POST['toID'] != '' and $message != '') {
+					if ($_POST['toID'] != '' and !empty($message)) {
 						$query = $this->db->prepare($this->admin->sendMessage);
 						$query->execute(array('content' => $message, 'fromID' => UserSession::userid(), 'toID' => $_POST['toID']));
 					}
@@ -53,42 +57,50 @@ class MessagesPage implements \Framework5\IExecutable {
 			}
 		}
 		
-		$contactVanityURL = \Framework5\Request::segment(1);
-		if (isset($contactVanityURL) and $contactVanityURL != '') {
+		
+		# conversation selected
+		$conversationUser = \Framework5\Request::segment(1);
+		if (isset($conversationUser) and !empty($conversationUser)) {
 			import('wddsocial.model.WDDSocial\UserVO');
-			$data = array('vanityURL' => $contactVanityURL);
+			$data = array('vanityURL' => $conversationUser);
 			$query = $this->db->prepare($this->sql->getUserByVanityURL);
 			$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\UserVO');
 			$query->execute($data);
 			$contact = $query->fetch();
 		}
+		
 		else {
 			$contact = false;
 		}
 		
-		# get contacts
-		$conversations = array();
-		import('wddsocial.model.WDDSocial\ConversationVO');
+		# list conversations
 		$query = $this->db->prepare($this->sql->getConversations);
 		$query->execute(array('id' => UserSession::userid()));
+		import('wddsocial.model.WDDSocial\ConversationVO');
 		$query->setFetchMode(\PDO::FETCH_CLASS,'WDDSocial\ConversationVO');
+		
+		$conversations = array();
 		while ($item = $query->fetch()) {
-			array_push($conversations,$item);
+			array_push($conversations, $item);
 		}
 		
-		$messages = array();
+		
 		# get messages
 		if ($contact) {
 			$query = $this->db->prepare($this->sql->getConversation);
-			$query->execute(array('currentUserID' => UserSession::userid(), 'contactID' => $contact->id));
+			$query->execute(
+				array('currentUserID' => UserSession::userid(), 'contactID' => $contact->id));
 			$query->setFetchMode(\PDO::FETCH_OBJ);
+			
+			$messages = array();
 			while ($msg = $query->fetch()) {
-				array_push($messages,$msg);
+				array_push($messages, $msg);
 			}
 		}
 		
 		# begin content
-		$content = render(':section', array('section' => 'begin_content', 'classes' => array('messages')));
+		$content = render(':section', 
+			array('section' => 'begin_content', 'classes' => array('messages')));
 		
 		# conversations
 		$content.= render(':section', 
@@ -98,7 +110,8 @@ class MessagesPage implements \Framework5\IExecutable {
 			'header' => $this->lang->text('conversations-header'))
 		);
 		
-		$content.= render('wddsocial.view.messages.WDDSocial\ConversationsView', array('conversations' => $conversations, 'error' => $conversationsError));
+		$content.= render('wddsocial.view.messages.WDDSocial\ConversationsView', 
+			array('conversations' => $conversations, 'error' => $conversationsError));
 		
 		$content.= render(':section', array('section' => 'end_content_section'));
 		
@@ -107,22 +120,27 @@ class MessagesPage implements \Framework5\IExecutable {
 			array('section' => 'begin_content_section',
 			'id' => 'conversation', 
 			'classes' => array('medium', 'no-margin'),
-			'header' => $this->lang->text('conversation-header', "{$contact->firstName} {$contact->lastName}"))
+			'header' => $this->lang->text(
+				'conversation-header', "{$contact->firstName} {$contact->lastName}"))
 		);
 		
-		if (!$contact) {
+		
+		
+		if ($contact) {
+			$content .= render('wddsocial.view.messages.WDDSocial\ConversationView', 
+				array('user' => $contact, 'messages' => $messages));
+		}
+		
+		else {
 			$content .= render('wddsocial.view.messages.WDDSocial\ChooseConversation');
 		}
-		else {
-			$content .= render('wddsocial.view.messages.WDDSocial\ConversationView', array('user' => $contact, 'messages' => $messages));
-		}
+		
 		
 		$content.= render(':section', array('section' => 'end_content_section'));
-		
-		# end content
 		$content.= render(':section', array('section' => 'end_content'));
 		
 		# display page
-		echo render(':template', array('title' => $this->lang->text('page-title'), 'content' => $content));
+		echo render(':template', 
+			array('title' => $this->lang->text('page-title'), 'content' => $content));
 	}
 }
