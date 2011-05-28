0b254e69d05272147f7ee372217d5fd6c0984466
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-28 06:35:35 -0400

    Fixed mixup: edited incorrect controller.

--- framework5/dev/controller/Formatter.php
+++ framework5/dev/controller/Formatter.php
@@ -3,29 +3,13 @@
 namespace Framework5\Dev;
 
 /*
-* Content Formatter
 * 
-* @author Tyler Matthews (tmatthewsdev@gmail.com)
+* @author tmatthews (tmatthewsdev@gmail.com)
 */
 
 class Formatter extends \Framework5\StaticController {
 	
-	/**
-	* Format a timestamp into a readable date
-	*/
-	
-	public static function format_timestamp($timestamp) {
+	public function date_format($timestamp) {
 		return date("F j, Y, g:i a", $timestamp);
-	}
-	
-	
-	
-	/**
-	* Formats user input
-	* Changes links into anchor tags
-	*/
-	
-	public static function format_user_content($text) {
-		return $text;
 	}
 }
---
+++ framework5/wddsocial/controller/Formatter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WDDSocial;
+
+/*
+* Content Formatter
+* 
+* @author Tyler Matthews (tmatthewsdev@gmail.com)
+*/
+
+class Formatter extends \Framework5\StaticController {
+	
+	/**
+	* Format a timestamp into a readable date
+	*/
+	
+	public static function format_timestamp($timestamp) {
+		return date("F j, Y, g:i a", $timestamp);
+	}
+	
+	
+	
+	/**
+	* Formats user input
+	* Changes links into anchor tags
+	*/
+	
+	public static function format_user_content($text) {
+		return $text;
+	}
+}
