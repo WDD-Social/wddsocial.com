62d3436fbcc1616a6411caef497ec4f5120d279e
Tyler Matthews <tmatthewsdev@gmail.com>
2011-05-28 06:33:41 -0400

    Created Formatter controller to format output content from data into human readable form

--- framework5/dev/controller/Formatter.php
+++ framework5/dev/controller/Formatter.php
@@ -3,6 +3,7 @@
 namespace Framework5\Dev;
 
 /*
+* Content Formatter
 * 
 * @author Tyler Matthews (tmatthewsdev@gmail.com)
 */
@@ -10,10 +11,21 @@ namespace Framework5\Dev;
 class Formatter extends \Framework5\StaticController {
 	
 	/**
-	* Determines what type of content to render
+	* Format a timestamp into a readable date
 	*/
 	
-	public static function format_time($timestamp) {
+	public static function format_timestamp($timestamp) {
 		return date("F j, Y, g:i a", $timestamp);
+	}
+	
+	
+	
+	/**
+	* Formats user input
+	* Changes links into anchor tags
+	*/
+	
+	public static function format_user_content($text) {
+		return $text;
 	}
 }
