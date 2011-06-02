249d978fb505c1666219f740e6e3b21bc3fda0f4
Anthony Colangelo <me@acolangelo.com>
2011-06-02 15:28:02 -0400

    Corrected cron path

--- framework5/wddsocial/controller/cron/DeleteOldICSCron.php
+++ framework5/wddsocial/controller/cron/DeleteOldICSCron.php
@@ -4,7 +4,7 @@ $pdo = new PDO("mysql:host=localhost;dbname=wddsocial", "root", "root");
 $query = $pdo->query("SELECT icsUID FROM events AS e WHERE TIMESTAMPDIFF(MINUTE,NOW(),endDateTime) < 0");
 $query->setFetchMode(PDO::FETCH_OBJ);
 while ($event = $query->fetch()) {
-	$file = "files/ics/wddsocial.{$event->icsUID}.ics";
+	$file = realpath("../../../../html/files/ics/wddsocial.{$event->icsUID}.ics");
 	if (file_exists($file)) {
 		unlink($file);
 	}
