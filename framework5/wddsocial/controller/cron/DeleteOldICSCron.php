6cc003df019990c2bf66482d387a13134d1721f7
Anthony Colangelo <me@acolangelo.com>
2011-06-02 15:33:15 -0400

    Fixed cron location

--- framework5/wddsocial/controller/cron/DeleteOldICSCron.php
+++ framework5/wddsocial/controller/cron/DeleteOldICSCron.php
@@ -4,7 +4,7 @@ $pdo = new PDO("mysql:host=localhost;dbname=wddsocial", "root", "root");
 $query = $pdo->query("SELECT icsUID FROM events AS e WHERE TIMESTAMPDIFF(MINUTE,NOW(),endDateTime) < 0");
 $query->setFetchMode(PDO::FETCH_OBJ);
 while ($event = $query->fetch()) {
-	$file = realpath("../../../../html/files/ics/wddsocial.{$event->icsUID}.ics");
+	$file = "../../../../html/files/ics/wddsocial.{$event->icsUID}.ics";
 	if (file_exists($file)) {
 		unlink($file);
 	}
@@ -12,3 +12,4 @@ while ($event = $query->fetch()) {
 		echo "<p>$file not found</p>";
 	}
 }
+echo "DOCUMENT ROOT {$_SERVER['DOCUMENT_ROOT']}";
