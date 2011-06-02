c915785b26f8a2185339683872616f72c3683b5f
Anthony Colangelo <me@acolangelo.com>
2011-06-02 15:19:32 -0400

    Updated cron job

--- framework5/wddsocial/controller/cron/DeleteOldICSCron.php
+++ framework5/wddsocial/controller/cron/DeleteOldICSCron.php
@@ -4,8 +4,11 @@ $pdo = new PDO("mysql:host=localhost;dbname=wddsocial", "root", "root");
 $query = $pdo->query("SELECT icsUID FROM events AS e WHERE TIMESTAMPDIFF(MINUTE,NOW(),endDateTime) < 0");
 $query->setFetchMode(PDO::FETCH_OBJ);
 while ($event = $query->fetch()) {
-	if (file_exists("files/ics/wddsocial.{$event->icsUID}.ics")) {
-		unlink("files/ics/wddsocial.{$event->icsUID}.ics");
+	$file = "files/ics/wddsocial.{$event->icsUID}.ics";
+	if (file_exists($file)) {
+		unlink($file);
+	}
+	else {
+		echo "<p>$file not found</p>";
 	}
 }
-echo "Cron happened.";
