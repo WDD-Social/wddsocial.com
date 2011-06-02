6c451e1b55eb58e6d9a7ceb4a121f44297a21e59
Anthony Colangelo <me@acolangelo.com>
2011-06-02 15:34:30 -0400

    Added real path to cron job

--- framework5/wddsocial/controller/cron/DeleteOldICSCron.php
+++ framework5/wddsocial/controller/cron/DeleteOldICSCron.php
@@ -4,7 +4,7 @@ $pdo = new PDO("mysql:host=localhost;dbname=wddsocial", "root", "root");
 $query = $pdo->query("SELECT icsUID FROM events AS e WHERE TIMESTAMPDIFF(MINUTE,NOW(),endDateTime) < 0");
 $query->setFetchMode(PDO::FETCH_OBJ);
 while ($event = $query->fetch()) {
-	$file = "../../../../html/files/ics/wddsocial.{$event->icsUID}.ics";
+	$file = "/home/112587/domains/wddsocial.com/html/files/ics/wddsocial.{$event->icsUID}.ics";
 	if (file_exists($file)) {
 		unlink($file);
 	}
